9093e8e099bd9226a162e0b4943c877b2f7221d1
Rosie <72038261+wenjunRose@users.noreply.github.com>
2020-11-02 19:37:10 -0500

    first unity project

---
+++ Unity Project/Assets/Scripts/Switch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Switch : MonoBehaviour
+{
+  public void readRule()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  public void readCharacter()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+  }
+
+  public void playGame()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+  }
+
+  public void ruleBack()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+  }
+
+  public void characterBack()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+  }
+
+  public void playBack()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+  }
+}
