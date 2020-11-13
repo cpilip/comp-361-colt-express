c4175bcece7d70549a232ea25a1f965f92f74e84
Rosie <72038261+wenjunRose@users.noreply.github.com>
2020-11-13 11:55:41 -0500

    Add sound effect when click
    All the buttons have sound effect

---
+++ ColtExpress_Unity/Assets/Scripts/SoundEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour
+{
+  public AudioSource soundEffect;
+  public AudioClip clickEffect;
+
+  public void ClickSound(){
+    soundEffect.PlayOneShot(clickEffect);
+  }
+}
