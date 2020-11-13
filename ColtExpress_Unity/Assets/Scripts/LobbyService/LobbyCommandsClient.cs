20a9d76107ac409339d64c1ffc441b4c24ecc962
Milo <milosobral@gmail.com>
2020-11-12 20:21:22 -0500

    fixed compilation error, still needs testing

--- ColtExpress_Unity/Assets/Scripts/LobbyService/LobbyCommandsClient.cs
+++ ColtExpress_Unity/Assets/Scripts/LobbyService/LobbyCommandsClient.cs
@@ -10,84 +10,69 @@ public class LobbyCommandsClient
 {
     private string response;
 
-    public void signIn() 
+    public void signIn(MonoBehaviour caller, string user, string pass) 
     {
-        string user = username.GetComponent<TMP_InputField>().text;
-        string pass = password.GetComponent<TMP_InputField>().text;
+        // string pass = password.GetComponent<TMP_InputField>().text;
         string url = string.Format("http://127.0.0.1:4242/oauth/token?grant_type=password&username={0}&password={1}", user, pass);
 
         List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
         formData.Add(new MultipartFormDataSection("user_oauth_approval=true&_csrf=19beb2db-3807-4dd5-9f64-6c733462281b&authorize=true"));
 
-        StartCoroutine(postRequest(url, formData, true));
+        caller.StartCoroutine(postRequest(url, formData, true));
     }
 
-    public void debugOnline()
+    public void debugOnline(MonoBehaviour caller)
     {
         string url = "http://127.0.0.1:4242/api/online";
-        StartCoroutine(getRequest(url, false));
+        caller.StartCoroutine(getRequest(url, false));
     }
 
-    public void signOut()
+    public void signOut(MonoBehaviour caller, string token)
     {
         // TODO Get current token
-        string token = "INVALID_TOKEN";
-        string url = string.format("http://127.0.0.1:4242/oauth/active?access_token={0}", token);
-        StartCoroutine(deleteRequest(currToken));
+        string url = string.Format("http://127.0.0.1:4242/oauth/active?access_token={0}", token);
+        caller.StartCoroutine(deleteRequest(url, false));
     }
 
-    public void getRole()
+    public void getRole(MonoBehaviour caller, string token)
     {
         // TODO get current token
-        string token = "INVALID_TOKEN";
-        string url = string.format("http://127.0.0.1:4242/oauth/role?access_token={0}", token);
-        StartCoroutine(getRequest(url, false));
+        string url = string.Format("http://127.0.0.1:4242/oauth/role?access_token={0}", token);
+        caller.StartCoroutine(getRequest(url, false));
     }
 
-    public void getAllUsers()
+    public void getAllUsers(MonoBehaviour caller, string token)
     {
         // TODO get current token
-        string token = "INVALID_TOKEN";
-        string url = string.format("http://127.0.0.1:4242/api/users?access_token={0}", token);
-        StartCoroutine(getRequest(url, true));
+        string url = string.Format("http://127.0.0.1:4242/api/users?access_token={0}", token);
+        caller.StartCoroutine(getRequest(url, true));
     }
 
-    public void getUsername()
+    public void getUsername(MonoBehaviour caller, string token)
     {
-        string token = "INVALID_TOKEN";
-        string url = string.format("http://127.0.0.1:4242/oauth/username?access_token={0}", token);    
-        StartCoroutine(getRequest(url, true));
+        string url = string.Format("http://127.0.0.1:4242/oauth/username?access_token={0}", token);    
+        caller.StartCoroutine(getRequest(url, true));
     }
 
-    public void registerGameService()
+    public void registerGameService(MonoBehaviour caller, string location, int maxPlayers, int minPlayers, string name, string webSupport, string token)
     {
-        // Get all the necessary information
-        string location = "INVALID_LOCATION";
-        int maxPlayers = 5;
-        int minPlayers = 3;
-        string name = "INVALID_NAME";
-        string webSupport = true;
-        string requestData = string.Format("{\"location\": \"{0}}\",\"maxSessionPlayers\": \"{1}\",\"minSessionPlayers\": \"{2}\",\"name\": \"{3}\",\"webSupport\": \"{4}\"}", location, maxPlayers, minPLayers, name, webSupport);
-        
-        string token = "INVALID_TOKEN";
+        string requestData = string.Format("{\"location\": \"{0}}\",\"maxSessionPlayers\": \"{1}\",\"minSessionPlayers\": \"{2}\",\"name\": \"{3}\",\"webSupport\": \"{4}\"}", location, maxPlayers, minPlayers, name, webSupport);
         string url = string.Format("http://127.0.0.1:4242/api/gameservices/{0}}?access_token={1}", name, token);
 
-        StartCoroutine(putRequest(url, requestData, true));
+        caller.StartCoroutine(putRequest(url, requestData, true));
     }
 
-    public void unregisterGameService() 
+    public void unregisterGameService(MonoBehaviour caller, string name, string token) 
     {
-        string name = "INVALID_NAME";
-        string token = "INVALID_TOKEN";
         string url = string.Format("http://127.0.0.1:4242/api/gameservices/{0}?access_token={1}", name, token);
 
-        StartCoroutine(deleteRequest(url, true));
+        caller.StartCoroutine(deleteRequest(url, true));
     }
 
 
     private IEnumerator getRequest(string url, bool auth) 
     {
-        using (UnityWebRequest webRequest = UnityWebRequest.Post(url))
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
         {
             if (auth)
             {
@@ -112,7 +97,7 @@ public class LobbyCommandsClient
     // Private methods that handle requests
     private IEnumerator deleteRequest(string token, bool auth)
     {
-        string url = string.format("http://127.0.0.1:4242/oauth/active?access_token={0}", token);
+        string url = string.Format("http://127.0.0.1:4242/oauth/active?access_token={0}", token);
         using (UnityWebRequest webRequest = UnityWebRequest.Delete(url))
         {
             if (auth) 
@@ -165,7 +150,7 @@ public class LobbyCommandsClient
     private IEnumerator putRequest(string url, string data, bool auth)
     {
         // Configure the data portion of the post request 
-        using (UnityWebRequest webRequest = UnityWebRequest.Post(url, data))
+        using (UnityWebRequest webRequest = UnityWebRequest.Put(url, data))
         {
             if (auth) 
             {
