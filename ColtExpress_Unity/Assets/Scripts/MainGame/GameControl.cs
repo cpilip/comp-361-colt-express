87829c95bbe8ac8d9f6aa30a215bd441e12857b0
Rosie <72038261+wenjunRose@users.noreply.github.com>
2020-11-12 22:14:06 -0500

    Update script

---
+++ ColtExpress_Unity/Assets/Scripts/MainGame/GameControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public GameObject GameMenu;
+    public void OpenMenu(){
+      if (GameMenu!=null){
+        bool isActive = GameMenu.activeSelf;
+        GameMenu.SetActive(!isActive);
+      }
+    }
+}
