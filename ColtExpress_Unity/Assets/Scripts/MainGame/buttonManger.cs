d6d1eab319cd527d4dd9c365f1b054e3378f1fe6
lukas1332 <72038314+lukas1332@users.noreply.github.com>
2021-03-07 15:19:50 +0800

    button in maingame

---
+++ ColtExpress_Unity/Assets/Scripts/MainGame/buttonManger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class buttonManger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject forward;
+    public GameObject backward;
+    public lu fm;
+    public lu bm;
+    public GameObject parent;
+   
+ 
+    void Update()
+    {
+
+        if ((fm.hasSet || bm.hasSet) == true)
+        {
+            return;
+
+        }
+        else
+        {
+           
+            Transform a = parent.transform;
+            try { 
+             if (a.GetChild(0) != null)
+            {
+                forward.SetActive(true);
+                backward.SetActive(true);
+                    fm.hasSet = false;
+                    bm.hasSet = false;
+
+            }
+        }
+            catch (Exception e) {
+                return;
+            }
+
+        }
+    }
+}
--- ColtExpress_Unity/Assets/Scripts/MainGame/lu.cs
+++ ColtExpress_Unity/Assets/Scripts/MainGame/lu.cs
@@ -8,21 +8,32 @@ public class lu : MonoBehaviour
     public GameObject parent;
     public GameObject train;
     public GameObject player;
+    public GameObject parentButton;
+    public GameObject other1;
+    public bool hasSet = false;
+    
     public void check() {
-        /* if (parent.transform.GetChild(0).gameObject != null) {
-             GameObject col=GameObject.Instantiate(parent.transform.GetChild(0).gameObject);
-             col.SetParent(parent.transorm);
-         }*/
+  
         Transform a = parent.transform;
+ 
         if (a.GetChild(0)!=null) {
-            /* Transform b = a.GetChild(0);
-             GameObject c = b.gameObject;
-             GameObject d = GameObject.Instantiate(c);
-             d.transform.SetParent(parent.transform);*/
-            Transform b = train.transform;
-            Transform c = b.GetChild(1);
-            player.transform.SetParent(c);
+            if (gameObject.name == "fowardMan") {
+
+                Transform b = train.transform;
+                Transform c = b.GetChild(2);
+                player.transform.SetParent(c);
+            } else if (gameObject.name == "backwordMan") {
+                Transform b = train.transform;
+                Transform c = b.GetChild(0);
+                player.transform.SetParent(c);
+            }
+           
+            
         }
+        parentButton.SetActive(false);
+        other1.SetActive(false);
+        hasSet = true;
+      
     }
 
     
