5aaf4f503e112825754daa22ebceeb0074b362e8
Mathieu Dufour <mathieu_dufour@hotmail.com>
2021-03-14 12:46:59 -0400

    Now listening on port 80

--- ServerColtExpv2/ServerColtExpv2/Program.cs
+++ ServerColtExpv2/ServerColtExpv2/Program.cs
@@ -11,8 +11,8 @@ class MyTcpListener
         TcpListener server = null;
         try
         {
-            // Set the TcpListener on port 13000.
-            Int32 port = 13000;
+            // TcpListener port.
+            Int32 port = 80;
             IPAddress localAddr = IPAddress.Parse("127.0.0.1");
             
 
