3bd30a885c66e4bf02fb69646de380a7c92ca8e2
cpilip <pilipchristina@gmail.com>
2021-03-16 09:38:49 -0400

    For demo

--- ServerColtExpv2/ServerColtExpv2/Program.cs
+++ ServerColtExpv2/ServerColtExpv2/Program.cs
@@ -87,7 +87,7 @@ class MyTcpListener
 
 
                     //Verify against lobby service
-                    if (clients.Count == 2)
+                    if (clients.Count == 3)
                     {
                         haveAllConnections = true;
                     }
