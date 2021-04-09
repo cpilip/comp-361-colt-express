d6de4fb29d4c52d3997ed78b8b4ac07a3b06c52d
Rosie <72038261+wenjunRose@users.noreply.github.com>
2021-04-09 20:15:03 +0800

    update

---
+++ ColtExpress_Unity/Assets/Scripts/CalculateScore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CalculateScore : MonoBehaviour
+{
+
+    public Text changeText;
+
+    void Start()
+    {
+        Debug.Log("Calculate the final score");
+
+        JObject o = JObject.Parse(data);
+        Boolean isEnd = endofCard();
+        //get all player's score
+
+        List<Character> r_P = o.SelectToken("r_players").ToObject<List<Character>>();
+        int score = o.SelectToken("finalScore").ToObject<int>();
+        string username = o.SelectToken("");
+
+
+        //how to call the CalculateGameScore();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+
+        //change the text from the finalScores scene
+        changeText.text = score;
+
+
+    }
+
+
+}
