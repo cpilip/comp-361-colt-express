34e9a5e6f1afb180eda5d0bdc5883b002fe103bb
Mathieu Dufour <mathieu_dufour@hotmail.com>
2021-04-11 16:12:09 -0400

    Copy login scripts

---
+++ ColtExpress_Unity/Assets/Scripts/LobbyService/Identification.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Identification : MonoBehaviour
+{
+    public string currToken;
+    public string refreshToken;
+
+    public void setToken(string token, string refToken)
+    {
+        this.currToken = token;
+        this.refreshToken = refToken;
+    }
+
+    public string getToken()
+    {
+        return this.currToken;
+    }
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
---
+++ ColtExpress_Unity/Assets/Scripts/LobbyService/LogIn.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+
+
+public class LogIn : MonoBehaviour
+{
+    public GameObject usernameField;
+    public GameObject passwordField;
+
+    public Object IDPrefab;
+
+    private LobbyCommandsClient LobbyCommands = new LobbyCommandsClient();
+
+    private string responseSoFar;
+
+    public void logIn() {
+        StartCoroutine(wait(1));
+    }
+
+    private IEnumerator wait(float time){
+        LobbyCommands.signIn(this, usernameField.GetComponent<TMP_InputField>().text, passwordField.GetComponent<TMP_InputField>().text);
+        // LobbyCommands.debugOnline(this);
+        GameObject id;
+        if (GameObject.Find("ID") == null) {
+            id = (GameObject)Instantiate(IDPrefab);
+            id.name = "ID";
+        } else {
+            id = GameObject.Find("ID");
+        }
+
+        string response = LobbyCommands.getResponse();
+        if (response == null) {
+            yield return new WaitForSeconds(time);
+            response = LobbyCommands.getResponse();
+        }
+        Debug.Log("Resp : "+ response);
+        SignInResponse json = JsonUtility.FromJson<SignInResponse>(response);
+        if (json.access_token == null){
+            Debug.Log("Sign In failed, try again");
+        } else {
+            Debug.Log(json.access_token);
+            Debug.Log(json.refresh_token);
+            id.GetComponent<Identification>().setToken(UnityWebRequest.EscapeURL(json.access_token), UnityWebRequest.EscapeURL(json.refresh_token));
+        }
+        
+    }
+}
